935544d36e0b7103c258854427f913099f9eb59d
root <teppalsateesh@gmail.com>
2020-09-30 12:08:00 +0530

    functions update

--- linked_list.py
+++ linked_list.py
@@ -32,11 +32,24 @@ class LinkedList:
         while last.next:
             last = last.next
         last.next = node
+    '''
+    this function add node after the given node
+    '''
+    def insertafter(self,prev_node,data):
+        node = Node(data)
+        if prev_node is None:
+            print("given previous node is not found")
+            return
+        node.next = prev_node.next
+        prev_node.next = node
     def print(self):
         t = self.head
+        c=0
         while t:
+            c+=1
             print(t.data,end=' ')
             t = t.next
+        print(" \n length of Linked List is",c)
 '''
 Main Function
 '''
@@ -52,5 +65,5 @@ if __name__ == "__main__":
     while m>0:
         l.append(int(input()))
         m -=1
-    print("Linked list:")
+    print("Linked lnist:")
     l.print()
