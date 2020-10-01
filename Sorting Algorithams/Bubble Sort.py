590870d24295c2bed22064c2844fa74a8aa067f1
root <teppalsateesh@gmail.com>
2020-10-01 21:15:15 +0530

    Sorting Algorithams

--- Sorting Algorithams/Bubble Sort.py
+++ Sorting Algorithams/Bubble Sort.py
@@ -1,9 +1,12 @@
 def BubbleSort(array):
     for i in range(len(array)):
+        swap = True
         for j in range(0,len(array)-i-1):
             if array[j] > array[j+1]:
                 array[j], array[j+1] = array[j+1],array[j]
-
+                swap = False
+        if swap:
+            break
 
 if __name__ == "__main__":
     data = list(map(int,input().split()))
