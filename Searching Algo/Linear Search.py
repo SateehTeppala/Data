a5ebdf60e34043a0a2d4c453765ac4102e78d6b2
TeppalaSateesh <teppalasateesh@gmail.com>
2020-11-16 11:19:11 +0530

    Searching

--- Searching Algo/Linear Search.py
+++ Searching Algo/Linear Search.py
@@ -1,15 +1,11 @@
-import random
-
-
 def Linear_Search(arr, k):
     for i in range(0, len(arr)):
         if (a[i] == k):
             return "Found"
     return "Not Found"
 
-
 if __name__ == "__main__":
-    a = random.sample(range(20, 40), 15)
+    a = [21,98,52,45,62,42,15,78]
     print(a)
     k = int(input())
     print(Linear_Search(a, k))
