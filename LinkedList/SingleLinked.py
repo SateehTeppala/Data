1cdf0c7917c7bc67422a3df4b0a95e4d3699014d
TeppalaSateesh <teppalasateesh@gmail.com>
2020-11-18 13:45:04 +0530

    insertAt() method

--- LinkedList/SingleLinked.py
+++ LinkedList/SingleLinked.py
@@ -10,12 +10,35 @@ class LinkedList:
     """docstring for LinkedList."""
     def __init__(self):
         self.head = None
-
+    def getLength(self):
+        currentnode = self.head
+        len = 0
+        while currentnode is not None:
+            currentnode = currentnode.next
+            len +=1
+        return len
     def insertBegin(self,newnode):
         temp = self.head
         self.head = newnode
         self.head.next = temp
         del temp
+    def insertAt(self,newnode,position):
+        currentnode = self.head
+        currentpostion = 0
+        if position == 0:
+            self.insertBegin(newnode)
+            return
+        if position < 0 or position > self.getLength():
+            print("Invalid position")
+            return
+        while  True:
+            if currentpostion == position:
+                previousnode.next = newnode
+                newnode.next = currentnode
+                break
+            previousnode = currentnode
+            currentnode = currentnode.next
+            currentpostion +=1
 
     def insertEnd(self,newnode):
         if self.head is None:
@@ -50,6 +73,8 @@ if __name__ == '__main__':
     ll.insertEnd(three)
     new = Node("New Head")
     ll.insertBegin(new)
+    at = Node(1)
+    ll.insertAt(at,3)
     ll.print()
     '''n = int(input("How many Nodes U want to insert..?:>"))
     ll = LinkedList()
